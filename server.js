d2d1ed433a17c011bcd2b4305724f2e8b311b0dd
Virtuous-Viraj <virajnerlekar1729@gmail.com>
2023-07-22 19:08:13 +0530

    deploy2

--- server.js
+++ server.js
@@ -21,5 +21,15 @@ app.get("/", (req, res) =>{
 })
 
 const port = process.env.PORT || 5000;
-
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
 app.listen(port, () => "server is running on port ")
